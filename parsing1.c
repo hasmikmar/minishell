1c69ed56db1980a61e8cda0269ec379904e895cc
hasmikmar <hmargaryan058@gmail.com>
2022-04-10 14:27:37 +0400

    [UPDATE]

--- parsing1.c
+++ parsing1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing1.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhachat <akhachat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hmargary <hmargary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 14:43:33 by akhachat          #+#    #+#             */
-/*   Updated: 2022/04/07 19:56:47 by akhachat         ###   ########.fr       */
+/*   Updated: 2022/04/10 14:27:25 by hmargary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	parse_args(char *s, int num, int i)
 
 	j = 1;
 	g_g.cmds[num].args = malloc(sizeof(char **) * ((ft_strlen(s) / 2) + 1));
-	(void)num;
-	printf("%s\n", g_g.cmds[num].name);
 	g_g.cmds[num].args[0] = g_g.cmds[num].name;
 	while (s[i])
 	{
@@ -27,7 +25,6 @@ void	parse_args(char *s, int num, int i)
 		g_g.cmds[num].args[j] = quote_handling(s, &i);
 		if (is_space(g_g.cmds[num].args[j]))
 			g_g.cmds[num].args[j] = NULL;
-		printf("%s\n", g_g.cmds[num].args[j]);
 		j++;
 	}
 	g_g.cmds[num].args[j] = NULL;
